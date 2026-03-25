d4988a4f8960052eb7d941bf272ab029c0314074
SharonLougheed <sharon.lougheed@gmail.com>
2026-03-24 22:22:36 -0500

    demo: add lint error and warning to show IDE extension problem

--- libs/libA/src/lib/componentA/LibA.ts
+++ libs/libA/src/lib/componentA/LibA.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { LibB } from '../../../../libB/src/lib/componentB/libB';
+
+// eslint should flag these, but the crash on the import above hides them
+const unused = 42;
+debugger;
 
 @Component({
   selector: 'lib-lib-a',
